f1a27e8f213dab447c9c8ebd4da62f70a34cea28
Ole <ole@vprsm.de>
2017-05-03 17:17:07 +0200

    Add send and receive functionality to Client

--- knx/client.go
+++ knx/client.go
@@ -9,7 +9,10 @@ import (
 //
 type Client struct {
 	sock    *Socket
+	channel byte
 	reaper  context.CancelFunc
+	seq     <-chan byte
+	ack     <-chan byte
 
 	//
 	Inbound <-chan []byte
@@ -80,6 +83,49 @@ func (client *Client) Close() {
 	client.sock.Close()
 }
 
+var (
+	ErrSendClosed   = errors.New("Outbound worker has terminated")
+	ErrSendRejected = errors.New("Gateway rejected tunnel request")
+	ErrSendTimeout  = errors.New("Gateway did not acknowledge tunnel request in time")
+)
+
+//
+func (client *Client) Send(data []byte) error {
+	seqNumber, open := <-client.seq
+	if !open {
+		return ErrSendClosed
+	}
+
+	req := &TunnelRequest{client.channel, seqNumber, data}
+	err := client.sock.Send(req)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < 5; i++ {
+		select {
+		case status, open := <-client.ack:
+			if !open {
+				return ErrSendClosed
+			}
+
+			if status == 0 {
+				return nil
+			} else {
+				return ErrSendRejected
+			}
+
+		case <-time.After(time.Second):
+			err := client.sock.Send(req)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return ErrSendTimeout
+}
+
 //
 func awaitConnectionResponse(sock *Socket) <-chan *ConnectionResponse {
 	resChan := make(chan *ConnectionResponse)
@@ -101,10 +147,15 @@ func awaitConnectionResponse(sock *Socket) <-chan *ConnectionResponse {
 func makeClient(sock *Socket, channel byte) *Client {
 	ctx, reaper := context.WithCancel(context.Background())
 
-	inbound := make(chan []byte)
-	go clientInboundWorker(ctx, reaper, sock, channel, inbound)
+	seq := make(chan byte)
+	ack := make(chan byte)
+	tunRes := make(chan *TunnelResponse)
+	go clientOutboundWorker(ctx, reaper, tunRes, seq, ack)
 
-	return &Client{sock, reaper, inbound}
+	inbound := make(chan []byte)
+	go clientInboundWorker(ctx, reaper, sock, channel, tunRes, inbound)
+
+	return &Client{sock, channel, reaper, seq, ack, inbound}
 }
 
 //
@@ -113,16 +164,20 @@ func clientInboundWorker(
 	reaper  context.CancelFunc,
 	sock    *Socket,
 	channel byte,
+	tunRes  chan<- *TunnelResponse,
 	inbound chan<- []byte,
 ) {
 	Logger.Printf("Client[%v]: Started inbound worker", sock.conn.RemoteAddr())
 	defer Logger.Printf("Client[%v]: Stopped inbound worker", sock.conn.RemoteAddr())
 
 	defer close(inbound)
+	defer reaper()
 
 	heartbeatTrigger := make(chan struct{})
 	stateResChan := make(chan *ConnectionStateResponse)
 	go clientHeartbeatWorker(ctx, reaper, sock, channel, heartbeatTrigger, stateResChan)
+
+	var incomingSeqNumber byte = 0
 
 	for {
 		select {
@@ -146,7 +201,6 @@ func clientInboundWorker(
 			// If the socket inbound channel is closed, this goroutine has no purpose.
 			if !open {
 				Logger.Printf("Client[%v]: Inbound channel has been closed", sock.conn.RemoteAddr())
-				reaper()
 				return
 			}
 
@@ -155,9 +209,105 @@ func clientInboundWorker(
 				res := payload.(*ConnectionStateResponse)
 
 				if res.Channel == channel {
-					stateResChan <- res
+					select {
+					case <-ctx.Done():
+						return
+
+					case stateResChan <- res:
+					}
+				}
+
+			case *TunnelRequest:
+				req := payload.(*TunnelRequest)
+
+				if req.Channel == channel {
+					// Acknowledge tunnel request
+					err := sock.Send(&TunnelResponse{channel, req.SeqNumber, 0})
+					if err != nil {
+						Logger.Printf("Client[%v]: Error while sending tunnel response: %v",
+						              sock.conn.RemoteAddr(), err)
+						return
+					}
+
+					// Relay to user if it fits the sequence
+					if req.SeqNumber == incomingSeqNumber {
+						Logger.Printf("Client[%v]: Inbound tunnel request: %v",
+						              sock.conn.RemoteAddr(), req.Payload)
+
+						select {
+						case <-ctx.Done():
+							return
+
+						case inbound <- req.Payload:
+						}
+
+						incomingSeqNumber++
+					}
+				}
+
+			case *TunnelResponse:
+				res := payload.(*TunnelResponse)
+
+				if res.Channel == channel {
+					select {
+					case <-ctx.Done():
+						return
+
+					case tunRes <- res:
+					}
 				}
 			}
+		}
+	}
+}
+
+//
+func clientOutboundWorker(
+	ctx     context.Context,
+	reaper  context.CancelFunc,
+	tunRes  <-chan *TunnelResponse,
+	seq     chan<- byte,
+	ack     chan<- byte,
+) {
+	defer reaper()
+	defer close(seq)
+	defer close(ack)
+
+	var seqNumber byte = 0
+
+	outerLoop:
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		// Client requests a sequence number because it wants to send something
+		case seq <- seqNumber:
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+
+				// Await tunnel response
+				case res := <-tunRes:
+					// We're only interested in the ones that match our sequence number
+					if res.SeqNumber == seqNumber {
+						select {
+						case <-ctx.Done():
+							return
+
+						// Send result of the tunnel request to the sender
+						case ack <- res.Status:
+							seqNumber++
+							continue outerLoop
+						}
+					}
+				}
+			}
+
+		// Discard out-of-cycle tunnel responses
+		case <-tunRes:
 		}
 	}
 }
@@ -169,7 +319,7 @@ func clientHeartbeatWorker(
 	sock    *Socket,
 	channel byte,
 	trigger <-chan struct{},
-	resChan <-chan *ConnectionStateResponse,
+	resChan <-chan *ConnectionStateResponse, // Really? Why not just 'byte'?
 ) {
 	Logger.Printf("Client[%v]: Started heartbeat worker", sock.conn.RemoteAddr())
 	defer Logger.Printf("Client[%v]: Stopped heartbeat worker", sock.conn.RemoteAddr())

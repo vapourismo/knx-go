c8fefd83d674172d0878ff70c628fcad267b13d1
Ole <ole@vprsm.de>
2017-05-10 16:21:43 +0200

    Refine tests for connHandle handleTunnelRequest

--- knx/client_test.go
+++ knx/client_test.go
@@ -435,17 +435,31 @@ func TestConnHandle_requestConnectionState(t *testing.T) {
 func TestConnHandle_handleTunnelRequest(t *testing.T) {
 	ctx := context.Background()
 
-	// Proper tunnel request from gateway.
-	t.Run("Ok", func (t *testing.T) {
+	t.Run("InvalidChannel", func (t *testing.T) {
 		sock := makeDummySocket()
-		inbound := make(chan []byte)
+		defer sock.Close()
+
+		var seqNumber uint8 = 0
+
+		conn := connHandle{sock, clientConfig, 1}
+		req := &TunnelRequest{2, 0, []byte{}}
+
+		err := conn.handleTunnelRequest(ctx, req, &seqNumber, make(chan []byte))
+		if err == nil {
+			t.Fatal("Should not succeed")
+		}
+	})
+
+	t.Run("InvalidSeqNumber", func (t *testing.T) {
+		sock := makeDummySocket()
 
 		const (
-			channel uint8 = 1
-			seqNumber uint8 = 0
+			channel       uint8 = 1
+			sendSeqNumber uint8 = 0
 		)
 
 		t.Run("Gateway", func (t *testing.T) {
+			defer sock.Close()
 			t.Parallel()
 
 			gw := gatewayHelper{ctx, sock, t}
@@ -453,127 +467,95 @@ func TestConnHandle_handleTunnelRequest(t *testing.T) {
 			msg := gw.receive()
 			if res, ok := msg.(*TunnelResponse); ok {
 				if res.Channel != channel {
-					t.Error("Mismatching channels")
+					t.Error("Mismatching channel")
 				}
 
-				if res.SeqNumber != seqNumber {
-					t.Error("Mismatching sequence numbers")
+				if res.SeqNumber != sendSeqNumber {
+					t.Error("Mismatching sequence number")
 				}
 
 				if res.Status != 0 {
 					t.Error("Invalid response status")
 				}
 			} else {
-				t.Fatalf("Unexpected incoming message type: %T", msg)
+				t.Fatal("Unexpected type %T", msg)
 			}
 		})
 
 		t.Run("Worker", func (t *testing.T) {
 			t.Parallel()
 
-			var seqNo uint8 = seqNumber
-			handle := &connHandle{sock, clientConfig, 1}
+			var seqNumber uint8 = sendSeqNumber + 1
 
-			req := &TunnelRequest{channel, seqNumber, []byte{}}
-			err := handle.handleTunnelRequest(ctx, req, &seqNo, inbound)
+			conn := connHandle{sock, clientConfig, channel}
+			req := &TunnelRequest{channel, sendSeqNumber, []byte{}}
+
+			err := conn.handleTunnelRequest(ctx, req, &seqNumber, make(chan []byte))
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			if seqNo != req.SeqNumber + 1 {
-				t.Error("Sequence number was not increased")
+			if seqNumber != sendSeqNumber + 1 {
+				t.Error("Sequence number was modified")
+			}
+		})
+	})
+
+	t.Run("Ok", func (t *testing.T) {
+		sock := makeDummySocket()
+		inbound := make(chan []byte)
+
+		const (
+			channel       uint8 = 1
+			sendSeqNumber uint8 = 0
+		)
+
+		t.Run("Gateway", func (t *testing.T) {
+			defer sock.Close()
+			t.Parallel()
+
+			gw := gatewayHelper{ctx, sock, t}
+
+			msg := gw.receive()
+			if res, ok := msg.(*TunnelResponse); ok {
+				if res.Channel != channel {
+					t.Error("Mismatching channel")
+				}
+
+				if res.SeqNumber != sendSeqNumber {
+					t.Error("Mismatching sequence number")
+				}
+
+				if res.Status != 0 {
+					t.Error("Invalid response status")
+				}
+			} else {
+				t.Fatal("Unexpected type %T", msg)
+			}
+		})
+
+		t.Run("Worker", func (t *testing.T) {
+			t.Parallel()
+
+			var seqNumber uint8 = sendSeqNumber
+
+			conn := connHandle{sock, clientConfig, channel}
+			req := &TunnelRequest{channel, sendSeqNumber, []byte{}}
+
+			err := conn.handleTunnelRequest(ctx, req, &seqNumber, inbound)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if seqNumber != sendSeqNumber + 1 {
+				t.Error("Sequence number has not been increased")
 			}
 		})
 
 		t.Run("Client", func (t *testing.T) {
 			t.Parallel()
 
-			select {
-			case <-ctx.Done():
-				t.Fatalf("While waiting for inbound packet: %v", ctx.Err())
-
-			case _, open := <-inbound:
-				if !open {
-					t.Fatal("Inbound channel was closed")
-				}
-			}
+			<-inbound
 		})
-	})
-
-	// Out-of-sequence tunnel request from the gateway.
-	t.Run("OutOfSequence", func (t *testing.T) {
-		sock := makeDummySocket()
-		inbound := make(chan []byte)
-
-		const (
-			channel uint8 = 1
-			seqNumber uint8 = 1
-		)
-
-		t.Run("Gateway", func (t *testing.T) {
-			t.Parallel()
-
-			gw := gatewayHelper{ctx, sock, t}
-
-			msg := gw.receive()
-			if res, ok := msg.(*TunnelResponse); ok {
-				if res.Channel != channel {
-					t.Error("Mismatching channels")
-				}
-
-				if res.SeqNumber != seqNumber {
-					t.Error("Mismatching sequence numbers")
-				}
-
-				if res.Status != 0 {
-					t.Error("Invalid response status")
-				}
-			} else {
-				t.Fatalf("Unexpected incoming message type: %T", msg)
-			}
-		})
-
-		t.Run("Worker", func (t *testing.T) {
-			t.Parallel()
-
-			var seqNo uint8 = seqNumber - 1
-			handle := &connHandle{sock, clientConfig, channel}
-
-			req := &TunnelRequest{channel, seqNumber, []byte{}}
-			err := handle.handleTunnelRequest(ctx, req, &seqNo, inbound)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if seqNo != seqNumber - 1 {
-				t.Error("Sequence number was changed by an out-of-sequence tunnel request")
-			}
-		})
-	})
-
-	// Tunnel request on incorrect channel.
-	t.Run("WrongChannel", func (t *testing.T) {
-		sock := makeDummySocket()
-		defer sock.Close()
-
-		inbound := make(chan []byte)
-
-		const (
-			channel uint8 = 1
-			seqNumber uint8 = 1
-		)
-
-		var seqNo uint8 = 0
-		handle := &connHandle{sock, clientConfig, channel + 1}
-
-		req := &TunnelRequest{channel, seqNumber, []byte{}}
-		err := handle.handleTunnelRequest(ctx, req, &seqNo, inbound)
-		if err == nil {
-			t.Fatal("Tunnel request with wrong channel has been successful")
-		}
-
-		if seqNo != 0 {
-			t.Error("Sequence number was changed by an invalid tunnel request")
-		}
 	})
 }

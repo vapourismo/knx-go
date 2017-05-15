4f4f59a2aa3b8d737aac958d2354561ba8854f73
Ole <ole@vprsm.de>
2017-05-15 15:29:07 +0200

    Clean TPDU stuff even further

--- knx/proto/tpdu.go
+++ knx/proto/tpdu.go
@@ -1,35 +1,9 @@
 package proto
 
 import (
+	"io"
 	"errors"
 )
-
-// A TPDU is the transport-layer protocol data unit within a L_Data frame.
-type TPDU []byte
-
-var (
-	ErrTransportDataTooShort = errors.New("Given TPDU is too short")
-	ErrTransportNotData      = errors.New("TPCI does not indicate a data packet")
-	ErrTransportNotControl   = errors.New("TPCI does not indicate a control packet")
-)
-
-// CheckTPDU validates the length of the given slice.
-func CheckTPDU(data []byte) (TPDU, error) {
-	if len(data) < 1 {
-		return nil, ErrTransportDataTooShort
-	}
-
-	tpdu := TPDU(data)
-
-	switch tpdu.PacketType() {
-	case UnnumberedDataPacket, NumberedDataPacket:
-		if len(data) < 2 {
-			return nil, ErrTransportDataTooShort
-		}
-	}
-
-	return tpdu, nil
-}
 
 // A TPCI is the transport-layer protocol control information (TPCI).
 type TPCI uint8
@@ -40,51 +14,6 @@ const (
 	UnnumberedControlPacket TPCI = 2
 	NumberedControlPacket   TPCI = 3
 )
-
-// PacketType returns the type of packet in the TPDU.
-func (tpdu TPDU) PacketType() TPCI {
-	return TPCI((tpdu[0] >> 6) & 3)
-}
-
-// SeqNumber retrieves the sequence number.
-func (tpdu TPDU) SeqNumber() uint8 {
-	return (tpdu[0] >> 2) & 15
-}
-
-// AppData parses the application-layer protocol data unit in order to provide control information
-// and the actual data.
-func (tpdu TPDU) AppData() (APCI, []byte, error) {
-	switch tpdu.PacketType() {
-	case UnnumberedDataPacket, NumberedDataPacket:
-		apci := APCI(((tpdu[0] & 3) << 2) | ((tpdu[1] >> 6) & 3))
-
-		var data []byte
-
-		if len(tpdu) > 2 {
-			data = make([]byte, len(tpdu) - 2)
-			copy(data, tpdu[2:])
-		} else {
-			data = make([]byte, 1)
-			data[0] = tpdu[1] & 63
-		}
-
-		return apci, data, nil
-
-	default:
-		return 0, nil, ErrTransportNotData
-	}
-}
-
-// ControlData retrieves the control data within the data unit.
-func (tpdu TPDU) ControlData() (uint8, error) {
-	switch tpdu.PacketType() {
-	case UnnumberedControlPacket, NumberedControlPacket:
-		return tpdu[0] & 3, nil
-
-	default:
-		return 0, ErrTransportNotControl
-	}
-}
 
 // An APCI is the application-layer protocol control information (APCI).
 type APCI uint8
@@ -107,3 +36,72 @@ const (
 	Restart                APCI = 14
 	Escape                 APCI = 15
 )
+
+// A TPDU is the transport-layer protocol data unit within a L_Data frame.
+type TPDU struct {
+	PacketType TPCI
+	SeqNumber  uint8
+	Control    uint8
+	Info       APCI
+	Data       []byte
+}
+
+var (
+	ErrTransportUnitTooShort = errors.New("Given TPDU is too short")
+)
+
+// ReadTPDU parses the given data in order to produce a TPDU struct.
+func ReadTPDU(data []byte) (*TPDU, error) {
+	if len(data) < 1 {
+		return nil, ErrTransportUnitTooShort
+	}
+
+	packetType := TPCI((data[0] >> 6) & 3)
+	seqNumber := (data[0] >> 2) & 15
+
+	switch packetType {
+	case UnnumberedControlPacket, NumberedControlPacket:
+		return &TPDU{packetType, seqNumber, data[0] & 3, 0, nil}, nil
+
+	case UnnumberedDataPacket, NumberedDataPacket:
+		if len(data) < 2 {
+			return nil, ErrTransportUnitTooShort
+		}
+
+		info := APCI((data[0] & 3) << 2 | (data[1] >> 6) & 3)
+
+		appData := make([]byte, len(data) - 1)
+		copy(appData, data[1:])
+
+		return &TPDU{packetType, seqNumber, 0, info, appData}, nil
+	}
+
+	return nil, errors.New("Unreachable")
+}
+
+// WriteTo writes the TPDU structure to the given Writer.
+func (tpdu TPDU) WriteTo(w io.Writer) (err error) {
+
+	buffer := []byte{
+		byte(tpdu.PacketType & 3) << 6 | byte(tpdu.SeqNumber & 15) << 2,
+	}
+
+	switch tpdu.PacketType {
+	case UnnumberedControlPacket, NumberedControlPacket:
+		buffer[0] |= byte(tpdu.Control & 3)
+
+	case UnnumberedDataPacket, NumberedDataPacket:
+		buffer[0] |= byte(tpdu.Info >> 2) & 3
+
+		if len(tpdu.Data) > 0 {
+			buffer = append(buffer, tpdu.Data...)
+			buffer[1] &= 63
+			buffer[1] |= byte(tpdu.Info & 3) << 6
+		} else {
+			buffer = []byte{buffer[0], byte(tpdu.Info & 3) << 6}
+		}
+	}
+
+	_, err = w.Write(buffer)
+	return
+}

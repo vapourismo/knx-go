f5e856da7170db881a3a9c5e205d7f05a22ee98f
Ole <ole@vprsm.de>
2017-05-16 15:28:06 +0200

    Add L_Data parser to proto sub-package

---
+++ knx/proto/ldata.go
@@ -0,0 +1,46 @@
+package proto
+
+import (
+	"io"
+	"github.com/vapourismo/knx-go/knx/binary"
+)
+
+// A LData is a link-layer data frame.
+type LData struct {
+	Control1    uint8
+	Control2    uint8
+	Source      uint16
+	Destination uint16
+	Data        TPDU
+}
+
+// ReadLData parses the given data in order to extract a LData frame.
+func ReadLData(r io.Reader) (*LData, error) {
+	ldata := &LData{}
+
+	err := binary.ReadSequence(
+		r, &ldata.Control1, &ldata.Control2, &ldata.Source, &ldata.Destination,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	tpdu, err := ReadTPDU(r)
+	if err != nil {
+		return nil, err
+	}
+
+	ldata.Data = *tpdu
+
+	return ldata, nil
+}
+
+// WriteTo writes the LData structure to the given Writer.
+func (ldata LData) WriteTo(w io.Writer) error {
+	err := binary.WriteSequence(w, ldata.Control1, ldata.Control2, ldata.Source, ldata.Destination)
+	if err != nil {
+		return err
+	}
+
+	return ldata.Data.WriteTo(w)
+}

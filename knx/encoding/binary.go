bfd067d4e2fad2c3bdcb73f51fb69de6edf24d21
Ole <ole@vprsm.de>
2017-05-19 16:00:24 +0200

    Change length type to int64

--- knx/encoding/binary.go
+++ knx/encoding/binary.go
@@ -6,54 +6,61 @@ import (
 	"io"
 )
 
-func writeUint8(w io.Writer, buffer []byte, value uint8) (int, error) {
+func writeUint8(w io.Writer, buffer []byte, value uint8) (int64, error) {
 	buffer[0] = value
-	return w.Write(buffer[:1])
+	len, err := w.Write(buffer[:1])
+	return int64(len), err
 }
 
-func writeUint16(w io.Writer, buffer []byte, value uint16) (int, error) {
+func writeUint16(w io.Writer, buffer []byte, value uint16) (int64, error) {
 	binary.BigEndian.PutUint16(buffer[:2], value)
-	return w.Write(buffer[:2])
+	len, err := w.Write(buffer[:2])
+	return int64(len), err
 }
 
-func writeUint32(w io.Writer, buffer []byte, value uint32) (int, error) {
+func writeUint32(w io.Writer, buffer []byte, value uint32) (int64, error) {
 	binary.BigEndian.PutUint32(buffer[:4], value)
-	return w.Write(buffer[:4])
+	len, err := w.Write(buffer[:4])
+	return int64(len), err
 }
 
-func writeUint64(w io.Writer, buffer []byte, value uint64) (int, error) {
+func writeUint64(w io.Writer, buffer []byte, value uint64) (int64, error) {
 	binary.BigEndian.PutUint64(buffer[:8], value)
-	return w.Write(buffer[:8])
+	len, err := w.Write(buffer[:8])
+	return int64(len), err
 }
 
-func writeUint16Slice(w io.Writer, values []uint16) (int, error) {
+func writeUint16Slice(w io.Writer, values []uint16) (int64, error) {
 	buffer := make([]byte, 2 * len(values))
 
 	for i, value := range values {
 		binary.BigEndian.PutUint16(buffer[i * 2:], value)
 	}
 
-	return w.Write(buffer)
+	len, err := w.Write(buffer)
+	return int64(len), err
 }
 
-func writeUint32Slice(w io.Writer, values []uint32) (int, error) {
+func writeUint32Slice(w io.Writer, values []uint32) (int64, error) {
 	buffer := make([]byte, 4 * len(values))
 
 	for i, value := range values {
 		binary.BigEndian.PutUint32(buffer[i * 4:], value)
 	}
 
-	return w.Write(buffer)
+	len, err := w.Write(buffer)
+	return int64(len), err
 }
 
-func writeUint64Slice(w io.Writer, values []uint64) (int, error) {
+func writeUint64Slice(w io.Writer, values []uint64) (int64, error) {
 	buffer := make([]byte, 8 * len(values))
 
 	for i, value := range values {
 		binary.BigEndian.PutUint64(buffer[i * 8:], value)
 	}
 
-	return w.Write(buffer)
+	len, err := w.Write(buffer)
+	return int64(len), err
 }
 
 //
@@ -61,7 +68,7 @@ var (
 	ErrUnknownDataType = errors.New("Unknown data type can't be written")
 )
 
-func writeItem(w io.Writer, item interface{}) (n int, err error) {
+func writeItem(w io.Writer, item interface{}) (int64, error) {
 	buffer := make([]byte, 8)
 
 	switch item := item.(type) {
@@ -83,14 +90,13 @@ func writeItem(w io.Writer, item interface{}) (n int, err error) {
 	case *int32:  return writeUint32(w, buffer, uint32(*item))
 	case *int64:  return writeUint64(w, buffer, uint64(*item))
 
-	case []uint8:  return w.Write(item)
+	case []uint8:  len, err := w.Write(item); return int64(len), err
 	case []uint16: return writeUint16Slice(w, item)
 	case []uint32: return writeUint32Slice(w, item)
 	case []uint64: return writeUint64Slice(w, item)
 
 	case io.WriterTo:
-		len, err := item.WriteTo(w)
-		return int(len), err
+		return item.WriteTo(w)
 
 	default:
 		return 0, ErrUnknownDataType
@@ -98,7 +104,7 @@ func writeItem(w io.Writer, item interface{}) (n int, err error) {
 }
 
 // WriteSequence writes items to the Writer.
-func WriteSequence(w io.Writer, items ...interface{}) (n int, err error) {
+func WriteSequence(w io.Writer, items ...interface{}) (n int64, err error) {
 	for _, item := range items {
 		len, err := writeItem(w, item)
 		n += len

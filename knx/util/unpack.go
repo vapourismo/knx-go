05752455c54f5cdd7b00773b7ffcb1c3ada8328b
Ole <ole@vprsm.de>
2017-06-05 17:09:59 +0200

    Add utility package

---
+++ knx/util/unpack.go
@@ -0,0 +1,110 @@
+package util
+
+import (
+	"fmt"
+	"io"
+)
+
+// Unpackable is implemented by types that can be initialized by reading from a byte slice.
+type Unpackable interface {
+	Unpack(data []byte) (uint, error)
+}
+
+func unpackUInt16(data []byte, output *uint16) (uint, error) {
+	if len(data) < 2 {
+		return 0, io.ErrUnexpectedEOF
+	}
+
+	*output = uint16(data[1]) | uint16(data[0])<<8
+
+	return 2, nil
+}
+
+func unpackUInt32(data []byte, output *uint32) (uint, error) {
+	if len(data) < 4 {
+		return 0, io.ErrUnexpectedEOF
+	}
+
+	*output = uint32(data[3]) | uint32(data[2])<<8 | uint32(data[1])<<16 | uint32(data[0])<<24
+
+	return 4, nil
+}
+
+func unpackUInt64(data []byte, output *uint64) (uint, error) {
+	if len(data) < 8 {
+		return 0, io.ErrUnexpectedEOF
+	}
+
+	*output = uint64(data[7]) | uint64(data[6])<<8 | uint64(data[5])<<16 | uint64(data[4])<<24 |
+		uint64(data[3])<<32 | uint64(data[2])<<40 | uint64(data[1])<<48 | uint64(data[0])<<56
+
+	return 8, nil
+}
+
+// Unpack the byte slice to the given value.
+func Unpack(data []byte, output interface{}) (uint, error) {
+	switch output := output.(type) {
+	case *uint8:
+		if len(data) < 1 {
+			return 0, io.ErrUnexpectedEOF
+		}
+
+		*output = data[0]
+		return 1, nil
+
+	case *int8:
+		if len(data) < 1 {
+			return 0, io.ErrUnexpectedEOF
+		}
+
+		*output = int8(data[0])
+		return 1, nil
+
+	case *uint16:
+		return unpackUInt16(data, output)
+
+	case *int16:
+		var u uint16
+		n, err := unpackUInt16(data, &u)
+		*output = int16(u)
+		return n, err
+
+	case *uint32:
+		return unpackUInt32(data, output)
+
+	case *int32:
+		var u uint32
+		n, err := unpackUInt32(data, &u)
+		*output = int32(u)
+		return n, err
+
+	case *uint64:
+		return unpackUInt64(data, output)
+
+	case *int64:
+		var u uint64
+		n, err := unpackUInt64(data, &u)
+		*output = int64(u)
+		return n, err
+
+	case Unpackable:
+		return output.Unpack(data)
+	}
+
+	return 0, fmt.Errorf("Can't unpack type %T", output)
+}
+
+// UnpackSome unpacks multiple values.
+func UnpackSome(data []byte, outputs ...interface{}) (uint, error) {
+	var n uint
+	for _, output := range outputs {
+		m, err := Unpack(data[n:], output)
+		n += m
+
+		if err != nil {
+			return n, err
+		}
+	}
+
+	return n, nil
+}
---
+++ knx/util/unpack_test.go
@@ -0,0 +1,171 @@
+package util
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"reflect"
+	"testing"
+)
+
+type unpackableTester [10]byte
+
+func (u *unpackableTester) Unpack(data []byte) (uint, error) {
+	if len(data) < 10 {
+		return 0, io.ErrUnexpectedEOF
+	}
+
+	return uint(copy(u[:], data)), nil
+}
+
+type unpackType struct {
+	typ       interface{}
+	length    uint
+	extractor func(data []byte) interface{}
+}
+
+func TestUnpack(t *testing.T) {
+	testCases := []unpackType{
+		{
+			typ:       uint8(0),
+			length:    1,
+			extractor: func(data []byte) interface{} { return data[0] },
+		},
+		{
+			typ:       int8(0),
+			length:    1,
+			extractor: func(data []byte) interface{} { return int8(data[0]) },
+		},
+		{
+			typ:       uint16(0),
+			length:    2,
+			extractor: func(data []byte) interface{} { return binary.BigEndian.Uint16(data) },
+		},
+		{
+			typ:       uint32(0),
+			length:    4,
+			extractor: func(data []byte) interface{} { return binary.BigEndian.Uint32(data) },
+		},
+		{
+			typ:       uint64(0),
+			length:    8,
+			extractor: func(data []byte) interface{} { return binary.BigEndian.Uint64(data) },
+		},
+		{
+			typ:       int16(0),
+			length:    2,
+			extractor: func(data []byte) interface{} { return int16(binary.BigEndian.Uint16(data)) },
+		},
+		{
+			typ:       int32(0),
+			length:    4,
+			extractor: func(data []byte) interface{} { return int32(binary.BigEndian.Uint32(data)) },
+		},
+		{
+			typ:       int64(0),
+			length:    8,
+			extractor: func(data []byte) interface{} { return int64(binary.BigEndian.Uint64(data)) },
+		},
+		{
+			typ:    unpackableTester{},
+			length: 10,
+			extractor: func(data []byte) interface{} {
+				var r unpackableTester
+				r.Unpack(data)
+				return r
+			},
+		},
+	}
+
+	num, err := Unpack(nil, nil)
+	if err == nil {
+		t.Error("Should not succeed")
+	}
+
+	if num != 0 {
+		t.Error("Should return 0 in case of error")
+	}
+
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("%T", testCase.typ), func(t *testing.T) {
+			ptrValue := reflect.New(reflect.TypeOf(testCase.typ))
+			ptr := ptrValue.Interface()
+
+			num, err := Unpack(nil, ptr)
+			if err != io.ErrUnexpectedEOF {
+				t.Fatal("Unexpected error:", err)
+			}
+
+			if num != 0 {
+				t.Error("Should return 0 in case of error")
+			}
+
+			buffer := make([]byte, testCase.length)
+
+			for i := 0; i < 100; i++ {
+				if i > 0 {
+					rand.Read(buffer)
+				}
+
+				num, err := Unpack(buffer, ptr)
+
+				if err != nil {
+					t.Errorf("Unexpected error: %v %v", err, buffer)
+				}
+
+				if num != testCase.length {
+					t.Errorf("Unexpected length: %v %v", num, buffer)
+				}
+
+				expected := testCase.extractor(buffer)
+				result := reflect.Indirect(ptrValue).Interface()
+
+				if !reflect.DeepEqual(expected, result) {
+					t.Errorf("Unexpected result: %v %v %v", result, expected, buffer)
+				}
+			}
+		})
+	}
+}
+
+type unpackableBad struct{}
+
+var errBadUnpack = errors.New("Bad unpack")
+
+func (unpackableBad) Unpack(data []byte) (uint, error) {
+	return 0, errBadUnpack
+}
+
+func TestUnpackSome(t *testing.T) {
+	num, err := UnpackSome(nil, unpackableBad{})
+	if err != errBadUnpack {
+		t.Error("Unexpected error:", err)
+	}
+
+	if num != 0 {
+		t.Error("Unexpected length:", num)
+	}
+
+	buffer := make([]byte, 25)
+
+	for i := 0; i < 100; i++ {
+		rand.Read(buffer)
+
+		var a uint8
+		var b uint16
+		var c uint32
+		var d uint64
+		var e unpackableTester
+
+		num, err := UnpackSome(buffer, &a, &b, &c, &d, &e)
+		if err != nil {
+			t.Errorf("Unexpected error: %v %v", err, buffer)
+		}
+
+		if num != uint(len(buffer)) {
+			t.Errorf("Unexpected length: %v %v", num, buffer)
+		}
+	}
+}

5531997e933c3ea841d07a9e52cda3aa455051ae
Ole <ole@vprsm.de>
2017-06-06 16:32:19 +0200

    Remove Read* functions from utilities

--- knx/encoding/binary.go
+++ knx/encoding/binary.go
@@ -30,29 +30,3 @@ func WriteSome(w io.Writer, items ...interface{}) (n int64, err error) {
 
 	return
 }
-
-// Read reads an item from the Reader.
-func Read(r io.Reader, item interface{}) (int64, error) {
-	if rf, ok := item.(io.ReaderFrom); ok {
-		return rf.ReadFrom(r)
-	}
-
-	if err := binary.Read(r, binary.BigEndian, item); err != nil {
-		return 0, err
-	}
-
-	return int64(binary.Size(item)), nil
-}
-
-// ReadSome reads multiple items from the Reader.
-func ReadSome(r io.Reader, items ...interface{}) (n int64, err error) {
-	for _, item := range items {
-		len, err := Read(r, item)
-		n += len
-		if err != nil {
-			return n, err
-		}
-	}
-
-	return
-}

f37eb025e416027008dfedf1d723041ea7cff309
Ole <ole@vprsm.de>
2017-06-14 13:20:32 +0200

    Simplify formats and datapoint types

--- knx/dpt/types.go
+++ knx/dpt/types.go
@@ -3,15 +3,21 @@
 package dpt
 
 import (
-	"errors"
 	"fmt"
 )
 
-// ErrInvalidLength is returned when the application data has unexpected length.
-var ErrInvalidLength = errors.New("Given application data has invalid length")
-
 // Switch is DPT 1.001.
 type Switch bool
+
+// Pack the datapoint value.
+func (sw Switch) Pack() []byte {
+	return packB1(bool(sw))
+}
+
+// Unpack the datapoint value from the given data.
+func (sw *Switch) Unpack(data []byte) error {
+	return unpackB1(data, (*bool)(sw))
+}
 
 // String generates a string representation.
 func (sw Switch) String() string {
@@ -22,52 +28,20 @@ func (sw Switch) String() string {
 	return "Off"
 }
 
-// Size returns the assembled size.
-func (Switch) Size() uint {
-	return sizeB1
-}
-
-// Pack assembles the datapoint value in the given buffer.
-func (sw Switch) Pack(buffer []byte) {
-	packB1(buffer, bool(sw))
-}
-
-// Unpack extracts the value from the application data.
-func (sw *Switch) Unpack(data []byte) (uint, error) {
-	if len(data) != sizeB1 {
-		return 0, ErrInvalidLength
-	}
-
-	unpackB1(data, (*bool)(sw))
-
-	return sizeB1, nil
-}
-
-// ValueTemp is DPT 9.0001.
+// ValueTemp is DPT 9.001.
 type ValueTemp float32
+
+// Pack the datapoint value.
+func (temp ValueTemp) Pack() []byte {
+	return packF16(float32(temp))
+}
+
+// Unpack the datapoint value from the given data.
+func (temp *ValueTemp) Unpack(data []byte) error {
+	return unpackF16(data, (*float32)(temp))
+}
 
 // String generates a string representation.
 func (temp ValueTemp) String() string {
 	return fmt.Sprintf("%.2f°C", float32(temp))
-}
-
-// Size returns the assembled size.
-func (ValueTemp) Size() uint {
-	return sizeF16
-}
-
-// Pack assembles the datapoint value in the given buffer.
-func (temp ValueTemp) Pack(buffer []byte) {
-	packF16(buffer, float32(temp))
-}
-
-// Unpack extracts the value from the application data.
-func (temp *ValueTemp) Unpack(data []byte) (uint, error) {
-	if len(data) != sizeF16 {
-		return 0, ErrInvalidLength
-	}
-
-	unpackF16(data, (*float32)(temp))
-
-	return sizeF16, nil
 }

fba731901768fc7f674d08d0aa014b58a5de87c5
Ole <ole@vprsm.de>
2017-06-19 13:12:08 +0200

    Add functions to generate GroupAddr and IndividualAddr

--- knx/cemi/address.go
+++ knx/cemi/address.go
@@ -9,6 +9,16 @@ import (
 // IndividualAddr is an address for a KNX device.
 type IndividualAddr uint16
 
+// NewIndividualAddr3 generates a group address with format a/b/c.
+func NewIndividualAddr3(a, b, c uint8) IndividualAddr {
+	return IndividualAddr(a&15)<<12 | IndividualAddr(b&15)<<8 | IndividualAddr(c)
+}
+
+// NewIndividualAddr2 generates a group address with format a/b.
+func NewIndividualAddr2(a, b uint8) IndividualAddr {
+	return IndividualAddr(a)<<8 | IndividualAddr(b)
+}
+
 // String generates a string representation.
 func (addr IndividualAddr) String() string {
 	return fmt.Sprintf("%d.%d.%d", uint8(addr>>12)&15, uint8(addr>>8)&15, uint8(addr))
@@ -16,6 +26,16 @@ func (addr IndividualAddr) String() string {
 
 // GroupAddr is an address for a KNX group object.
 type GroupAddr uint16
+
+// NewGroupAddr3 generates a group address with format a/b/c.
+func NewGroupAddr3(a, b, c uint8) GroupAddr {
+	return GroupAddr(a&31)<<11 | GroupAddr(b&7)<<8 | GroupAddr(c)
+}
+
+// NewGroupAddr2 generates a group address with format a/b.
+func NewGroupAddr2(a, b uint8) GroupAddr {
+	return GroupAddr(a)<<8 | GroupAddr(b)
+}
 
 // String generates a string representation.
 func (addr GroupAddr) String() string {

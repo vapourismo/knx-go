63d5fde23c392cc867d0a437df5b6291cbbc5eaf
Ole <ole@vprsm.de>
2017-06-21 23:42:47 +0200

    Add first draft for .knxproject format 1.1

---
+++ ets/knxproj11/archive.go
@@ -0,0 +1,111 @@
+// Copyright 2017 Ole Krüger.
+// Licensed under the MIT license which can be found in the LICENSE file.
+
+package knxproj11
+
+import (
+	"archive/zip"
+	"encoding/xml"
+	"fmt"
+	"path"
+	"regexp"
+)
+
+// projectMetaDocument is used in conjunction with xml.Unmarshal to extract the meta information
+// from a XML document.
+type projectMetaDocument struct {
+	Project struct {
+		ID                 string `xml:"Id,attr"`
+		ProjectInformation struct {
+			Name   string `xml:"Name,attr"`
+			Number uint   `xml:"ProjectId,attr"`
+		}
+	}
+}
+
+// ProjectMeta contains meta information for a project.
+type ProjectMeta struct {
+	file *zip.File
+	ID   string
+	Name string
+}
+
+// Process the project file in order to retrieve more information.
+func (pm *ProjectMeta) Process() (Project, error) {
+	return processProject(pm.file)
+}
+
+// A ProjectArchive is a handle to a .knxproj file.
+type ProjectArchive struct {
+	archive  *zip.ReadCloser
+	Projects []ProjectMeta
+}
+
+// projectFileRe is a regular expression that matches against project meta files.
+var projectFileRe = regexp.MustCompile("^(p|P)-([0-9a-zA-Z]+)/(p|P)roject.xml$")
+
+// readProjectMeta extracts project meta information from a file.
+func readProjectMeta(file *zip.File) (meta projectMetaDocument, err error) {
+	r, err := file.Open()
+	if err != nil {
+		return
+	}
+
+	err = xml.NewDecoder(r).Decode(&meta)
+	r.Close()
+
+	return
+}
+
+// Open a .knxproj file.
+func Open(file string) (*ProjectArchive, error) {
+	archive, err := zip.OpenReader(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var projects []ProjectMeta
+
+outerLoop:
+	for _, file := range archive.File {
+		// Is this a project meta file?
+		if projectFileRe.MatchString(file.Name) {
+			meta, err := readProjectMeta(file)
+			if err != nil {
+				archive.Close()
+				return nil, err
+			}
+
+			// Determine the file name which contains the actual project.
+			projectFile := path.Join(
+				path.Dir(file.Name),
+				fmt.Sprintf("%d.xml", meta.Project.ProjectInformation.Number),
+			)
+
+			for _, file := range archive.File {
+				if file.Name == projectFile {
+					projects = append(projects, ProjectMeta{
+						file: file,
+						ID:   meta.Project.ID,
+						Name: meta.Project.ProjectInformation.Name,
+					})
+
+					continue outerLoop
+				}
+			}
+
+			archive.Close()
+			return nil, fmt.Errorf("Could not find file: %s", projectFile)
+		}
+	}
+
+	return &ProjectArchive{
+		archive:  archive,
+		Projects: projects,
+	}, nil
+}
+
+// Close the handle.
+func (pz *ProjectArchive) Close() error {
+	return pz.archive.Close()
+}
---
+++ ets/knxproj11/project.go
@@ -0,0 +1,133 @@
+// Copyright 2017 Ole Krüger.
+// Licensed under the MIT license which can be found in the LICENSE file.
+
+package knxproj11
+
+import (
+	"archive/zip"
+	"encoding/xml"
+	"io/ioutil"
+
+	"github.com/vapourismo/knx-go/knx/cemi"
+)
+
+// GroupObject is a group object.
+type GroupObject struct {
+	ID      string         `xml:"Id,attr"`
+	Name    string         `xml:"Name,attr"`
+	Address cemi.GroupAddr `xml:"Address,attr"`
+}
+
+// GroupObjectRange is a range of group objects and ranges.
+type GroupObjectRange struct {
+	ID        string             `xml:"Id,attr"`
+	Name      string             `xml:"Name,attr"`
+	Objects   []GroupObject      `xml:"GroupAddress"`
+	SubRanges []GroupObjectRange `xml:"GroupRange"`
+}
+
+// ComObjectRef is a reference to a communcation object.
+type ComObjectRef struct {
+	RefID      string
+	SendIDs    []string
+	ReceiveIDs []string
+}
+
+// UnmarshalXML extracts the ComObject information.
+func (obj *ComObjectRef) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var doc struct {
+		RefID      string `xml:"RefId,attr"`
+		Connectors struct {
+			A []struct {
+				XMLName xml.Name
+				RefID   string `xml:"GroupAddressRefId,attr"`
+			} `xml:",any"`
+		}
+	}
+
+	if err := d.DecodeElement(&doc, &start); err != nil {
+		return err
+	}
+
+	obj.RefID = doc.RefID
+	obj.SendIDs = nil
+	obj.ReceiveIDs = nil
+
+	for _, con := range doc.Connectors.A {
+		switch con.XMLName.Local {
+		case "Send":
+			obj.SendIDs = append(obj.SendIDs, con.RefID)
+
+		case "Receive":
+			obj.ReceiveIDs = append(obj.ReceiveIDs, con.RefID)
+		}
+	}
+
+	return nil
+}
+
+// Device is a KNX device.
+type Device struct {
+	ID         string         `xml:"Id,attr"`
+	Name       string         `xml:"Name,attr"`
+	Address    uint           `xml:"Address,attr"`
+	ComObjects []ComObjectRef `xml:"ComObjectInstanceRefs>ComObjectInstanceRef"`
+}
+
+// Line is a KNX line.
+type Line struct {
+	ID      string   `xml:"Id,attr"`
+	Name    string   `xml:"Name,attr"`
+	Address uint     `xml:"Address,attr"`
+	Devices []Device `xml:"DeviceInstance"`
+}
+
+// Area is a KNX area.
+type Area struct {
+	ID      string `xml:"Id,attr"`
+	Name    string `xml:"Name,attr"`
+	Address uint   `xml:"Address,attr"`
+	Lines   []Line `xml:"Line"`
+}
+
+// Installation is an installation.
+type Installation struct {
+	ID     string             `xml:"InstallationId,attr"`
+	Name   string             `xml:"Name,attr"`
+	Areas  []Area             `xml:"Topology>Area"`
+	Groups []GroupObjectRange `xml:"GroupAddresses>GroupRanges>GroupRange"`
+}
+
+// Project is an ETS project.
+type Project struct {
+	ID            string         `xml:"Id,attr"`
+	Installations []Installation `xml:"Installations>Installation"`
+}
+
+// processProject
+func processProject(file *zip.File) (proj Project, err error) {
+	// Acquire a handle to the file.
+	r, err := file.Open()
+	if err != nil {
+		return
+	}
+
+	// Retrieve the contents of the file.
+	contents, err := ioutil.ReadAll(r)
+	r.Close()
+
+	if err != nil {
+		return
+	}
+
+	var doc struct {
+		Project Project
+	}
+
+	// Extract the information.
+	if err = xml.Unmarshal(contents, &doc); err != nil {
+		return
+	}
+
+	return doc.Project, nil
+}

a49c44e91da9df0146ed92c7efb6d13dba26834a
Ole <ole@vprsm.de>
2019-01-12 12:29:17 +0000

    Fix datapoint type 9.004

--- knx/dpt/types.go
+++ knx/dpt/types.go
@@ -263,33 +263,29 @@ func (d DPT_9001) String() string {
 type DPT_9004 float32
 
 func (d DPT_9004) Pack() []byte {
-	value := d
-
-	// Clip the value to a valid range
-	if value < 0 {
-		value = 0
+	if d <= 0 {
+		return packF16(0)
+	} else if d >= 670760 {
+		return packF16(670760)
+	} else {
+		return packF16(float32(d))
 	}
-	if value > 670760 {
-		value = 670760
-	}
-
-	return packF16(float32(value))
 }
 
 func (d *DPT_9004) Unpack(data []byte) error {
-	var buf float32
-
-	err := unpackF16(data, &buf)
-	if err == nil {
-		value := DPT_9004(buf)
-
-		// Check the value for valid range
-		if value > 670760 {
-			return fmt.Errorf("value \"%.2f\" outside range [0, 670760]", value)
-		}
-
-		*d = value
+	var value float32
+	if err := unpackF16(data, &value); err != nil {
+		return err
 	}
+
+	// Check the value for valid range
+	if value < 0 {
+		return fmt.Errorf("Temperatur \"%.2f\" outside range [0, 670760]", value)
+	} else if value > 670760 {
+		return fmt.Errorf("Temperatur \"%.2f\" outside range [0, 670760]", value)
+	}
+
+	*d = DPT_9004(value)
 
 	return nil
 }

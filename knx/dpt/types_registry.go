82afbd38b417c5e37fef3ba5277fbb9c621c5335
Sven Rebhan <Sven.Rebhan@googlemail.com>
2020-02-19 22:44:50 +0100

    Make the type-registry a singleton.

--- knx/dpt/types_registry.go
+++ knx/dpt/types_registry.go
@@ -4,94 +4,74 @@
 package dpt
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
+	"sync"
 )
 
-type Registry map[string]reflect.Type
+var (
+	types = [...]DatapointValue{
+		// 1.xxx
+		new(DPT_1001),
+		new(DPT_1002),
+		new(DPT_1003),
+		new(DPT_1009),
+		new(DPT_1010),
 
-// Fill the registry with the given datatype
-func (r Registry) add(d DatapointValue) error {
-	// Determine the name of the datatype
-	d_type := reflect.TypeOf(d)
-	name := d_type.Name()
-	if d_type.Kind() == reflect.Ptr {
-		d_type = d_type.Elem()
-		name = d_type.Name()
+		// 5.xxx
+		new(DPT_5001),
+		new(DPT_5003),
+		new(DPT_5004),
+
+		// 9.xxx
+		new(DPT_9001),
+		new(DPT_9004),
+		new(DPT_9005),
+		new(DPT_9007),
+
+		// 12.xxx
+		new(DPT_12001),
+
+		// 13.xxx
+		new(DPT_13001),
+		new(DPT_13002),
+		new(DPT_13010),
+		new(DPT_13011),
+		new(DPT_13012),
+		new(DPT_13013),
+		new(DPT_13014),
+		new(DPT_13015),
 	}
-
-	// Make sure we only handle DPT types^
-	if !strings.HasPrefix(name, "DPT_") {
-		return fmt.Errorf("invalid type \"%v\" for registry!", name)
-	}
-
-	// Convert the name into KNX yy.xxx (e.g. DPT_1001 --> 1.001) format
-	name = strings.TrimPrefix(name, "DPT_")
-	name = name[:len(name)-3] + "." + name[len(name)-3:]
-
-	// Register the type
-	r[name] = d_type
-
-	return nil
-}
+	once     sync.Once
+	registry map[string]reflect.Type
+)
 
 // Init function used to add all types
-func NewRegistry() (r *Registry, err error) {
-	r = &Registry{}
-
-	// Create a list of all known datapoint-types
-	dpts := make([]DatapointValue, 0)
-
-	// 1.xxx
-	dpts = append(dpts, new(DPT_1001))
-	dpts = append(dpts, new(DPT_1002))
-	dpts = append(dpts, new(DPT_1003))
-	dpts = append(dpts, new(DPT_1009))
-	dpts = append(dpts, new(DPT_1010))
-
-	// 5.xxx
-	dpts = append(dpts, new(DPT_5001))
-	dpts = append(dpts, new(DPT_5003))
-	dpts = append(dpts, new(DPT_5004))
-
-	// 9.xxx
-	dpts = append(dpts, new(DPT_9001))
-	dpts = append(dpts, new(DPT_9004))
-	dpts = append(dpts, new(DPT_9005))
-	dpts = append(dpts, new(DPT_9007))
-
-	// 12.xxx
-	dpts = append(dpts, new(DPT_12001))
-
-	// 13.xxx
-	dpts = append(dpts, new(DPT_13001))
-	dpts = append(dpts, new(DPT_13002))
-	dpts = append(dpts, new(DPT_13010))
-	dpts = append(dpts, new(DPT_13011))
-	dpts = append(dpts, new(DPT_13012))
-	dpts = append(dpts, new(DPT_13013))
-	dpts = append(dpts, new(DPT_13014))
-	dpts = append(dpts, new(DPT_13015))
-
+func setup() {
 	// Register the types
-	for _, d := range dpts {
-		err = r.add(d)
-		if err != nil {
-			return
-		}
-	}
+	registry = make(map[string]reflect.Type)
+	for _, d := range types {
+		// Determine the name of the datatype
+		d_type := reflect.TypeOf(d).Elem()
+		name := d_type.Name()
 
-	return
+		// Convert the name into KNX yy.xxx (e.g. DPT_1001 --> 1.001) format
+		name = name[4:len(name)-3] + "." + name[len(name)-3:]
+
+		// Register the type
+		registry[name] = d_type
+	}
 }
 
-func (r Registry) List() []string {
+func ListSupportedTypes() []string {
+	// Singleton, can only run once
+	once.Do(setup)
+
 	// Initialize the key-list
-	keys := make([]string, len(r))
+	keys := make([]string, len(registry))
 
 	// Fill the key-list
 	i := 0
-	for k := range r {
+	for k := range registry {
 		keys[i] = k
 		i++
 	}
@@ -99,8 +79,11 @@ func (r Registry) List() []string {
 	return keys
 }
 
-func (r Registry) Produce(name string) (d DatapointValue, ok bool) {
-	x, ok := r[name]
+func Produce(name string) (d DatapointValue, ok bool) {
+	// Singleton, can only run once
+	once.Do(setup)
+
+	x, ok := registry[name]
 
 	if ok {
 		d = reflect.New(x).Interface().(DatapointValue)

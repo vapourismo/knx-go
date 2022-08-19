9c9ad46baa090a78cd53dea513048b36eed1df14
Ole Krüger <ole@vprsm.de>
2022-08-19 20:22:46 +0100

    Add Nix shell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "knx-go";
+
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs;
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        packages =
+          # go tooling
+          (with pkgs; [
+            go
+            gopls
+          ])
+          ++
+          # Nix tooling
+          (with pkgs; [
+            nixpkgs-fmt
+            rnix-lsp
+          ]);
+      };
+    });
+}

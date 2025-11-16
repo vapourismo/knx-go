01548c0cfd2509fee1d1640b12d25cac6b42dc1a
userwithoutpassword <146129024+userwithoutpassword@users.noreply.github.com>
2025-11-16 17:10:52 +0100

    Update types_12.go

--- knx/dpt/types_12.go
+++ knx/dpt/types_12.go
@@ -2,6 +2,25 @@ package dpt
 
 import "fmt"
 
+// DPT_12 represents DPT 12.* / Unsigned (4-Byte).
+type DPT_12 uint32
+
+func (d DPT_12) Pack() []byte {
+	return packU32(uint32(d))
+}
+
+func (d *DPT_12) Unpack(data []byte) error {
+	return unpackU32(data, (*uint32)(d))
+}
+
+func (d DPT_12) Unit() string {
+	return ""
+}
+
+func (d DPT_12) String() string {
+	return fmt.Sprintf("", uint32(d))
+}
+
 // DPT_12001 represents DPT 12.001 / Unsigned counter.
 type DPT_12001 uint32
 
